2a9b0dfe19706d47d38f648dd93a991c4312e64c
Snigdha Sur <sur.snigdha@gmail.com>
2017-06-21 03:38:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  string = ""
+  i = 0
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    until i == array.length - 1
+      string << "#{array[i]}, "
+      i += 1
+    end
+    string << "and #{array[array.length-1]}"
+    return string
+  end
 end
